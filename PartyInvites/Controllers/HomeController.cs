c5d60dace499b7577140af018280393caa59f8fe
Marcos Xavier <marcos.desenvolvimento95@gmail.com>
2020-02-03 02:51:32 -0300

    Primeiro commit
    Primeiro commit para o git_hub.

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using PartyInvites.Models;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good morning" : "Good Afternoon";
+            return View("MyView");
+        }
+
+        [HttpGet]
+        public ViewResult RsvpForm()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ViewResult RsvpForm(GuestResponse guestResponse)
+        {
+            //Estou fazendo essa verificação, pois os meus campos são obrigatórios. 
+            if (ModelState.IsValid)
+            {
+                Repository.AddResponse(guestResponse);
+
+                //No retorno abaixo, está sendo chamada uma pagina Thanks e esta sendo populado nela o objeto guestResponse
+                return View("Thanks", guestResponse);
+            }
+            else
+            {
+                //Isso é um errro de validação
+                return View();
+            }
+        }
+
+        public ViewResult ListResponses()
+        {
+            return View(Repository.Responses.Where(r => r.WillAttend == true));
+        }
+
+    }
+}
